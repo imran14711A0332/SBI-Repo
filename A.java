907078809c49f162ad7dc8a007f65d9a4e5d7f2c
imran14711A0332 <59567961+imran14711A0332@users.noreply.github.com>
2020-01-06 23:19:45 +0530

    Add files via upload
    just for commit

---
+++ A.java
@@ -0,0 +1,12 @@
+class A
+{
+	public static void main(String[]args){
+	System.out.println("method overload1");
+	}
+	public static void main(String[]args){
+	System.out.println("method overload2");
+	}
+	
+}
+
+		
