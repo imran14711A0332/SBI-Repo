c529f494b18b635da251e9f0fd8e01388b43fd8f
imran14711A0332 <shaikimran14711A0332@gmail.com>
2020-01-07 21:26:13 +0530

    removet1

---
+++ A.java
@@ -0,0 +1,14 @@
+class A
+{
+	public static void main(String[]args){
+	System.out.println("method overload1");
+	}
+	public static void main(String[]args){
+	System.out.println("method overload2");
+	}
+public void m1(){
+}
+	
+}
+
+		
